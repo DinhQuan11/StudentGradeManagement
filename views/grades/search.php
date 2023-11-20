1739b9429d97895c02fad06e29f6ef643a792611
Quân Võ Nguyễn Đình <quanvonguyendinh@outlook.com>
2023-11-21 01:49:58 +0700

    add icon and block user access

--- views/grades/search.php
+++ views/grades/search.php
@@ -1,4 +1,7 @@
-<?php $dirRoot = strstr(__DIR__, '\views', true) ?>
+<?php
+$dirRoot = strstr(__DIR__, '\views', true);
+require_once $dirRoot . '/controllers/Login.php'
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -6,6 +9,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="icon" type="image/x-icon" href="/assets/imgs/logo.svg" />
   <title>Quản lý điểm</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
   <link rel="stylesheet" href="/assets/css/style.css">
